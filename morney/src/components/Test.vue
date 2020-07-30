e9c8839e5db13501c51ab63c665bd5309177afbf
Yang <651033755@qq.com>
2020-07-30 22:46:38 +0800

    add a new program

---
+++ morney/src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="about">
+        <h1>This </h1>
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
