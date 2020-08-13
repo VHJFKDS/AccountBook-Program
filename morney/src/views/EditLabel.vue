aec85f7f80d70529dfb00bdaf4716e4b0947bd37
Yang <651033755@qq.com>
2020-08-13 23:17:36 +0800

    将tag的string改为{id，name}

---
+++ morney/src/views/EditLabel.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+      编辑标签
+  </Layout>
+</template>
+
+<script lang="ts">
+import Layout from '@/components/Layout.vue';
+
+export default {
+  name: 'app',
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
