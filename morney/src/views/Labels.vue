2c64da84ac5a36c93bbf3c76d7e6ea9ffd905b96
Yang <651033755@qq.com>
2020-08-13 22:50:29 +0800

    展示和创建标签

--- morney/src/views/Labels.vue
+++ morney/src/views/Labels.vue
@@ -1,18 +1,79 @@
 <template>
-
     <Layout>
-      <p>labels.vue</p>
+      <ol class="tags">
+        <li v-for="tag in tags" :key="tag"> 
+          <span>{{tag}}</span>
+          <Icon name="right"/>
+        </li>
+     
+      </ol>
+      <div class="createTag-wrapper">
+        <button class="createTag" @click="createTag">新建标签</button>
+      </div>
     </Layout>
     
 </template>
 
 <script lang="ts">
+import Vue from 'vue'
+import Icon from '@/components/Icon.vue';
+import {Component} from 'vue-property-decorator';
+import tagListModel from '../models/tagListModel';
 
-export default {
- name:'Labels',
+tagListModel.fetch()
+
+@Component
+export default class Labels extends Vue{
+ tags = tagListModel.data
+
+ createTag(){
+   const name = window.prompt('请输入标签名')
+   if(name){
+     const message = tagListModel.create(name)
+     if(message === 'duplicated'){
+       window.alert('标签名重复了')
+     }else if(message === 'success'){
+       window.alert('添加成功！')
+     }
+   }
+ }
+
+
 }
 </script>
 
 <style lang="scss" scoped>
+
+.tags{
+  background: white;
+  font-size: 16px;
+  padding-left: 16px;
+  li{
+    min-height: 44px;
+    border-bottom: 1px solid #e6e6e6;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    svg{
+      width: 18px;
+      height: 18px;
+      color: #666;
+      margin-right: 16px;
+    }
+  }
+}
+.createTag{
+  background: #767676;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  padding: 0 16px;
+  &-wrapper{    //父元素
+    padding: 16px;
+    text-align: center;
+    margin-top: 44-16px;
+  }
+}
 
 </style>
--- morney/src/views/money.vue
+++ morney/src/views/money.vue
@@ -15,16 +15,18 @@ import Types from "@/components/money/Types.vue"
 import Notes from "@/components/money/Notes.vue"
 import Tags from "@/components/money/Tags.vue"
 import { Component, Watch } from "vue-property-decorator";
-import model from '../model';
+import recordListModel from '../models/recordListModel';
+import tagListModel from '../models/tagListModel';
 
-const recordList = model.fetch();
+const recordList = recordListModel.fetch();
+const tagList = tagListModel.fetch(); 
 
 @Component({
   components:{Tags,NumberPad,Types,Notes}
 })
 
 export default class Money extends Vue{
- tags=['衣','食','住','行','菜谱'];
+ tags= tagList
  recordList:RecordItem[] = recordList;
  record:RecordItem = {  //记录
   tags:[],notes:'',type:'-',amount:0
@@ -37,13 +39,13 @@ export default class Money extends Vue{
   this.record.notes = value
  }
 saveRecord(){  //点了ok后的数据存进去
-const record2:RecordItem = model.clone(this.record)  
+const record2:RecordItem = recordListModel.clone(this.record)  
 record2.createdAt = new Date()
   this.recordList.push(record2)
 }
 @Watch('recordList')
 onRecordListChange(){
-  model.save(this.recordList)
+  recordListModel.save(this.recordList)
 }
 
 }
@@ -51,10 +53,10 @@ onRecordListChange(){
 
 <style lang="scss">
 .layout-content{
+  background: white;
   flex-grow: 1;
   display: flex;
   flex-direction: column-reverse;
- 
   }
 
 </style>
