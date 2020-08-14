6432af9d6bc5423dedbd589e9fabd04cb694f69f
Yang <651033755@qq.com>
2020-08-14 10:45:05 +0800

    更正EditLabel组件

--- morney/src/views/EditLabel.vue
+++ morney/src/views/EditLabel.vue
@@ -10,17 +10,21 @@ import Layout from '@/components/Layout.vue';
 import { Component } from 'vue-property-decorator';
 import tagListModel from '../models/tagListModel';
 
+tagListModel.fetch();
+
 @Component
 export default class EditLabel extends Vue{
+  created(){
   const id = this.$route.params.id;
-  tagListModel.fetch()
-  const tags = tagListModel.data
+  const tags = tagListModel.data;
   const tag = tags.filter(t => t.id === id)[0];
   if(tag){
     console.log(tag)
   }else{
     this.$router.replace('/404')
   }
+  }
+  
 }
 </script>
 <style lang="scss" scoped>
